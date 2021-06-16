debe2ea40c79b563128650a72b563b0f7bec67a8
mydogspies <mydogspies@zoho.com>
2021-06-16 11:29:27 +0200

    The initial setup for the enter word page

---
+++ app/src/main/java/com/mydogspies/guess_a_word/model/Word.java
@@ -0,0 +1,24 @@
+package com.mydogspies.guess_a_word.model;
+
+
+public class Word {
+
+    private String word;
+    private String[] wordArray;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String[] getWordArray() {
+        return wordArray;
+    }
+
+    public void setWordArray(String[] wordArray) {
+        this.wordArray = wordArray;
+    }
+}
